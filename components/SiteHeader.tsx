82341ebcd5c760fe65bac535114b82ec27d53dbf
Raphael Ram <ramraphael@gmail.com>
2021-08-01 16:58:56 -0400

    fix: semantic tag for header link

--- components/SiteHeader.tsx
+++ components/SiteHeader.tsx
@@ -1,6 +1,7 @@
 import { FunctionComponent, Fragment } from "react";
 import { useRouter } from "next/router";
 import cn from "classnames";
+import Link from "next/link";
 
 export const SiteHeader: FunctionComponent = () => {
   const router = useRouter();
@@ -9,31 +10,31 @@ export const SiteHeader: FunctionComponent = () => {
 
   return (
     <Fragment>
-      <div
-        role="link"
-        onClick={() => router.push("/")}
-        className="cursor-pointer mb-12 group text-gray-700 hover:opacity-80 transition-opacity duration-200"
-        style={{ width: "fit-content" }}
-      >
-        <h1
-          className={cn(
-            { "text-6xl": isHomePage },
-            { "text-5xl": !isHomePage },
-            "font-bold",
-            "mb-4"
-          )}
+      <Link href="/">
+        <a
+          className="block cursor-pointer mb-12 group text-gray-700 hover:opacity-80 transition-opacity duration-200"
+          style={{ width: "fit-content" }}
         >
-          <span className="text-red-700">Raphael</span> Ram{" "}
-          {!isHomePage && "writes"}
-        </h1>
+          <h1
+            className={cn(
+              { "text-6xl": isHomePage },
+              { "text-5xl": !isHomePage },
+              "font-bold",
+              "mb-4"
+            )}
+          >
+            <span className="text-red-700">Raphael</span> Ram{" "}
+            {!isHomePage && "writes"}
+          </h1>
 
-        {isHomePage && (
-          <h2 className="text-3xl">
-            <span className="text-red-700">writes</span> poetry, prose, and{" "}
-            <span className="text-red-700">code</span>.
-          </h2>
-        )}
-      </div>
+          {isHomePage && (
+            <h2 className="text-3xl">
+              <span className="text-red-700">writes</span> poetry, prose, and{" "}
+              <span className="text-red-700">code</span>.
+            </h2>
+          )}
+        </a>
+      </Link>
       <hr className="mb-12" />
     </Fragment>
   );
